9a8247228031a25a3a68d2cfd710781d053f5850
Abdulmajeed-Abdulrahman <7majeed10@gmail.com>
2023-09-25 13:34:27 +0300

    selectionSort

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
+    public static void selectionSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = a[minIndex];
+            a[minIndex] = a[i];
+            a[i] = temp;
+        }
 }

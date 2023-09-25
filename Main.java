57d27a8bee2d4658bb2fa1183da4a0492a1a0774
Mohammed.Alotaibi <76397375+Mohammed0291@users.noreply.github.com>
2023-09-25 13:37:59 +0300

    insertionSort(Array)

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
+package Lab03;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        System.out.println("Original Array:");
+        printArray(arr);
 
+        insertionSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
     }
+
     public static void selectionSort(int[] a) {
         int n = a.length;
         for (int i = 0; i < n - 1; i++) {
@@ -15,4 +25,28 @@ public class Main {
             a[minIndex] = a[i];
             a[i] = temp;
         }
+    }
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+    public static void printArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
 }
+
